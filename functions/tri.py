303d6c9d146cdaba1d4414c0e2ca0ca5bd8b1c47
Enes Küçük <eneskck2f@hotmail.com>
2025-02-20 12:19:47 +0300

    Fonksiyon bitti.

---
+++ functions/tri.py
@@ -0,0 +1,10 @@
+a = int(input("Bir taban değeri giriniz: "))
+b = int(input("Bir yükseklik değeri giriniz: "))
+
+def tri_area(base, height):
+	area = (base * height) / 2
+	return area
+
+print(tri_area(a,b))
+	
+	 
---
+++ main.py/main.py
@@ -0,0 +1,13 @@
+num = int(input("Bir sayı giriniz: "))
+def Fizz_buzz(num):
+    if num%3==0 and num%5==0:
+        return("Fizz Buzz")
+    elif num%5==0:
+        return("Buzz")
+    elif num%3==0:
+        return("Fizz")
+    else:
+        return(str(num))
+    
+x = Fizz_buzz(num)
+print(x)
